bd707e32808ba4a8ef2817645be75b4fe8e06d75
MrNoober <qubus2011@gmail.com>
2024-08-12 16:14:39 +0200

    new layer

--- js/layers.js
+++ js/layers.js
@@ -61,7 +61,7 @@ addLayer("p", {
         },
         16: {
             title: "Selfboost",
-            description: "Boosts prestige point gain via points",
+            description: "Points boost themselves",
             cost: new Decimal(64),
             effect() {
                 return player.points.add(1).log(3).add(1).pow(1.6)
@@ -109,4 +109,34 @@ addLayer("p", {
             cost: new Decimal(1e24),
         },
     },
+})
+
+addLayer("r", {
+    name: "rebirth", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "FFFFFF",
+    requires: new Decimal(1000000), // Can be a function that takes requirement increases into account
+    resource: "rebirth points", // Name of prestige currency
+    baseResource: "prestige points", // Name of resource prestige is based on
+    baseAmount() {return player.prestigepoints}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.1, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown() {return (hasUpgrade('p',31))},
+
 })
--- js/mod.js
+++ js/mod.js
@@ -19,8 +19,8 @@ let VERSION = {
 
 let changelog = `<h1>Changelog:</h1><br>
 	<h3>v0.0</h3><br>
-		- Added something.<br>
-		- Added something.`
+		- Fixed "Selfboost" description.<br>
+		- Added a new layer.`
 
 let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
 

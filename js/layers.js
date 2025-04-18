ba1cfbb15a596e2055c76ebec2d81999a6f7353a
MrNoober <qubus2011@gmail.com>
2025-04-18 14:48:33 +0200

    j

--- js/layers.js
+++ js/layers.js
@@ -1,22 +1,20 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("m", {
+    name: "math", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "M", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
     color: "#00FFFF",
-    requires: new Decimal(5), // Can be a function that takes requirement increases into account
+    requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "static", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
-        if (hasUpgrade('p',25)) mult = mult.times(3)
-        if (hasUpgrade('p',32)) mult = mult.times(2)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
@@ -24,11 +22,11 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the f
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "m", description: "P: Reset for math essence", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown() {return true}
     ,
-    passiveGeneration() { return (hasUpgrade("p", 23)?0.05:0)  },
+    passiveGeneration() { },
     branches: [],
     upgrades: {
         11: {
@@ -106,3 +104,4 @@ addLayer("p", {
         },
     },
 })
+
